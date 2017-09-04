5a88b0f613c366318ab2c07367d3698cf9f7df20
Won Maung Thein <wonmaungthein@gmail.com>
2017-09-04 22:07:50 +0100

    deleted model, server.js and unnecessary files

--- routes/index.js
+++ routes/index.js
@@ -19,23 +19,24 @@ router.get('/', function (req, res, next) {
   dbClient.getproducts({}, callBack)
 });
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+
+/* GET products/lorem page. */
+router.get('/products/lorem', (req, res) => {
   const callBack = (error, products) => {
     if (error) {
       res.sendStatus(500)
     }
     else {
       res.render('index', {
-        title: 'AcmeInc',
-        description: 'We sell the finest goods and services.',
-        products,
+        title: String,
+        cost : Number, 
+        rating: Number,
+        comments: [{}],
       });
     }
   }
   dbClient.getproducts({}, callBack)
 });
-
 
 
 
--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-http.createServer((req,res)=> {
-    response.writeHead (200)
-response.end ('hello world')
-}).listen (8081);
-
-console.log ('server running at 3000')
