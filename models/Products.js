d396c1304e2a14ca52c4a606efd2fc4ac6083ea6
kharazzoro <kha.1991.14.2017@gmail.com>
2017-09-04 22:09:00 +0100

    Schema and Model for Products (#13)
    * create schema models without comments
    
    * Add models products schema do not contain comment schema

---
+++ models/Products.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const schema = new Schema({
+    urlPath: String,
+    title: String,
+    price: Number,
+    rating: String,
+    commentCount: Number,
+    isTopRated: Boolean
+});
+
+
+
+const Products = mongoose.model('products', schema);
+
+module.exports = Products;
