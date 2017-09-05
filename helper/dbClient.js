00ccd04df034b110885c4930974dc34c6da472de
Won Maung Thein <wonmaungthein@gmail.com>
2017-09-05 18:10:01 +0100

    added mongodb link

--- helper/dbClient.js
+++ helper/dbClient.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const mongoConnection = process.env.MONGODB_URI || 'mongodb://localhost:27017/shopcentre';
-
+const mongoConnection = process.env.MONGODB_URI ||'mongodb://shopcentre:@products-shard-00-00-k8pvi.mongodb.net:27017,products-shard-00-01-k8pvi.mongodb.net:27017,products-shard-00-02-k8pvi.mongodb.net:27017/admin?replicaSet=products-shard-0&ssl=true'
 const getproducts = (query, sucessCallBack) => {
     MongoClient.connect(mongoConnection, (error, db) => {
         const cursor = db.collection("products").find(query);
