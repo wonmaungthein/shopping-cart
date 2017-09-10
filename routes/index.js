b88f6ac3a5f1afa2ae7e496a90203daa58b9acd1
Won Maung Thein <wonmaungthein@gmail.com>
2017-09-10 18:45:32 +0100

    Added more details in single product hbs

--- routes/index.js
+++ routes/index.js
@@ -33,11 +33,7 @@ router.get('/products/:urlPath', function (req, res, next) {
         title: products[0].title,
         description: `We sell the finest goods and services. 
         This is the ${products[0].title}.`,
-<<<<<<< HEAD
-        product: products[0],
-=======
         product: products[0]
->>>>>>> 9b05a0fa277906785919b477918bfe8f3124a9fd
       });
     }
   }
