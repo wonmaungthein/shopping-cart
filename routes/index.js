70ee1686dbbc672a13095b29584ba05051c5306a
kharazzoro <kha.1991.14.2017@gmail.com>
2017-09-11 12:50:07 +0100

    Add nodemon autoreload (#31)
    * update
    
    * add modemon so npm run dev work

--- routes/index.js
+++ routes/index.js
@@ -4,42 +4,39 @@ const dbClient = require('../helper/dbClient.js');
 
 /* GET home page. */
 
-router.get('/', function (req, res, next) {
-  const callBack = (error, products) => {
-    if (error) {
-      res.sendStatus(500)
+router.get('/', function(req, res, next) {
+    const callBack = (error, products) => {
+        if (error) {
+            res.sendStatus(500)
+        } else {
+            res.render('index', {
+                title: 'AcmeInc',
+                description: 'We sell the finest goods and services.',
+                products,
+            });
+        }
     }
-    else {
-      res.render('index', {
-        title: 'AcmeInc',
-        description: 'We sell the finest goods and services.',
-        products,
-      });
-    }
-  }
-  dbClient.getproducts({}, callBack)
+    dbClient.getproducts({}, callBack)
 });
 
 /* GET single-product information page. */
 
-router.get('/products/:urlPath', function (req, res, next) {
-  const urlPath = req.params.urlPath;
-  const callBack = (error, products) => {
-    if (error) {
-      res.sendStatus(500)
-    }
-    else {
-      res.render('single-product', {
-        title: products[0].title,
-        description: `We sell the finest goods and services. 
+
+router.get('/products/:urlPath', function(req, res, next) {
+    const urlPath = req.params.urlPath;
+    const callBack = (error, products) => {
+        if (error) {
+            res.sendStatus(500)
+        } else {
+            res.render('single-product', {
+                title: products[0].title,
+                description: `We sell the finest goods and services. 
         This is the ${products[0].title}.`,
-        product: products[0]
-      });
+                product: products[0]
+            });
+        }
     }
-  }
-  dbClient.getproducts({ urlPath }, callBack);
+    dbClient.getproducts({ urlPath }, callBack);
 });
 
 module.exports = router;
-
-
